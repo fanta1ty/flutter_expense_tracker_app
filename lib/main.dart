8f0e23a24cc250b91b38de3b9cdfd46d8d006d6f
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-02-06 13:54:11 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expense_tracker_app/expenses.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: ...,
+      home: Expenses(),
     ),
   );
 }
